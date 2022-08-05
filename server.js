41c77696911240d3f19516905b0b019c6366ddc8
Darshan Pagar <mrdarshanpagar@gmail.com>
2022-08-06 00:53:16 +0530

    setup folder structure

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const articleRouter = require('./routes/articles')
+const app = express()
+app.set('view engine','ejs');
+app.use('/articles',articleRouter)
+
+app.get('/',(req, res)=>{
+    res.render('index')
+})
+app.listen(8000)
