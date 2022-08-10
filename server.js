7efe2d4965d8731242ca1a8aeeeac0b8a25f7060
Darshan Pagar <mrdarshanpagar@gmail.com>
2022-08-11 01:05:57 +0530

    added delete button for deleting articles

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express')
 const mongoose = require('mongoose')
 const articleRouter = require('./routes/articles')
 const Article = require('./models/article')
+const methodOverride = require('method-override')
 const app = express()
 mongoose.connect('mongodb://localhost/blog')
 
 app.set('view engine','ejs');
 app.use(express.urlencoded({extended: false}))
-
+app.use(methodOverride('_method'))
 
 app.get('/', async(req, res)=>{
     const articles = await Article.find().sort({createdAt: 'desc'})
